847119e78cf0f9177a220bdcbbbe8c6e0a74ff39
Giuseppe D'Arrò <giuseppedarro.gd@gmail.com>
2025-10-16 00:56:06 +0200

    updated README.md

--- waiter-app/Comanda/app/build.gradle.kts
+++ waiter-app/Comanda/app/build.gradle.kts
@@ -52,6 +52,12 @@ dependencies {
     implementation(libs.androidx.compose.material3)
     implementation(libs.androidx.navigation.compose)
     implementation(libs.androidx.material.icons.extended)
+
+    // Koin
+    implementation(libs.koin.core)
+    implementation(libs.koin.android)
+    implementation(libs.koin.androidx.compose)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
