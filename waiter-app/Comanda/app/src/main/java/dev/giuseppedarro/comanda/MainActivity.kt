39012d7d40ce500f1f450003b8e0386d071e5afd
Giuseppe D'Arrò <giuseppedarro.gd@gmail.com>
2026-02-04 22:37:45 +0100

    Added splash screen and changed its color

--- waiter-app/Comanda/app/src/main/java/dev/giuseppedarro/comanda/MainActivity.kt
+++ waiter-app/Comanda/app/src/main/java/dev/giuseppedarro/comanda/MainActivity.kt
@@ -8,11 +8,13 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import dev.giuseppedarro.comanda.core.navigation.NavGraph
 import dev.giuseppedarro.comanda.ui.theme.ComandaTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
